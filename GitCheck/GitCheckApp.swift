c51f297e4035a57ce1849db1efd6806ca387edf1
Francois <nickonair@web.de>
2023-04-21 09:06:24 +0200

    Initial Commit

---
+++ GitCheck/GitCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitCheckApp.swift
+//  GitCheck
+//
+//  Created by Student on 21.04.23.
+//
+
+import SwiftUI
+
+@main
+struct GitCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
